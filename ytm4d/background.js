6e1b086b2ae97629a89529c4d1f9bb793becfe32
dh6k <87336618+dh6k@users.noreply.github.com>
2025-04-27 00:52:08 +0700

    Add files via upload

--- ytm4d/background.js
+++ ytm4d/background.js
@@ -44,7 +44,7 @@ chrome.runtime.onInstalled.addListener(() => {
         requestHeaders: [{
           header: "User-Agent",
           operation: "set",
-          value: "Android 12"
+          value: "Mozilla/5.0 (Linux; Android 15) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.7049.95 Mobile Safari/537.36"
         }]
       },
       condition: {
